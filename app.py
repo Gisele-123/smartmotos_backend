8b2a38b3c6a4f165727ebdf4e17dfb61659108d6
Gisele-123 <giselemigisha53@gmail.com>
2025-04-28 21:58:06 +0200

    Configure Port Binding for Render

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 from config import Config
 from extensions import db, init_twilio
 from flask_migrate import Migrate
@@ -35,4 +36,6 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Use Render's PORT or default to 5000
+    app.run(host='0.0.0.0', port=port) 
+    # app.run(debug=True)

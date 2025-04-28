06bd14222e76554707955994bf3290926f16a972
Gisele-123 <giselemigisha53@gmail.com>
2025-04-28 19:41:06 +0200

    new packages

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
+# build.sh
 #!/bin/bash
-# Install build dependencies first
 pip install --upgrade pip
-pip install setuptools wheel
 pip install grpcio-tools==1.71.0
-# Then install the rest
 pip install -r requirements.txt

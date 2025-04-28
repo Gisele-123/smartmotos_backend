33917ba119b0d20d633d1839e3ed0b4daab9a69f
Gisele-123 <giselemigisha53@gmail.com>
2025-04-28 19:36:42 +0200

    new requirement.txt

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Install build dependencies first
-pip install grpcio-tools>=1.62.0
+pip install --upgrade pip
+pip install setuptools wheel
+pip install grpcio-tools==1.71.0
 # Then install the rest
 pip install -r requirements.txt
